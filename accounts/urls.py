39ba014e345c870d8f590a0b325d3fdb1ba3396e
beak jong seoung <jjong015189@gmail.com>
2024-07-23 11:48:10 +0900

    회원가입, 프로필 기능 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,13 @@
-# from django.urls import path
-# from . import views
+from django.urls import path
 
-urlpatterns = []
+from .views import LoginView, LogoutView, ProfileUpdateView, SignupView, profile
+
+app_name = "accounts"
+
+urlpatterns = [
+    path("signup/", SignupView.as_view(), name="signup"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("profile/", profile, name="profile"),
+    path("profile/edit/", ProfileUpdateView.as_view(), name="profile_edit"),
+]
